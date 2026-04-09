f862cbe14a6f207a1a8f5a819d58e664cf8fc27b
nmpra <251479110+nmpra@users.noreply.github.com>
2026-04-09 18:41:16 +0700

    add min length on schemas

--- schemas.py
+++ schemas.py
@@ -1,9 +1,9 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from typing import Literal
 
 class VehicleCreate(BaseModel):
-    name: str
-    cc: int
-    license_plate: str
+    name: str = Field(min_length=3)
+    cc: int = Field(min_length=2)
+    license_plate: str = Field(min_length=3)
     vehicle_type: str | Literal["Car", "Motorcycle"]
     transmission: str | Literal["Manual", "Semi Auto", "Automatic"]
