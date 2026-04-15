fc3e02a4501535ec21302d5fc1cf46dd3e07a59b
nmpra <251479110+nmpra@users.noreply.github.com>
2026-04-15 13:11:44 +0700

    add endpoint for oil and maintenance update

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from database import engine, get_db
 models.Base.metadata.create_all(bind=engine)
 app = FastAPI()
 
-@app.post("/vehicles/", response_model=schemas.VehicleResponse)
+@app.post("/vehicles/", response_model=schemas.VehicleResponse, tags=["Vehicles"])
 def create_vehicle(vehicle: schemas.VehicleCreate, db: Session = Depends(get_db)):
     db_vehicle = models.Vehicle(**vehicle.model_dump())
     db.add(db_vehicle)
@@ -16,14 +16,23 @@ def create_vehicle(vehicle: schemas.VehicleCreate, db: Session = Depends(get_db)
     db.refresh(db_vehicle)
     return db_vehicle
 
-@app.get("/vehicles/{vehicle_id}", response_model=schemas.VehicleResponse)
+@app.get("/vehicles/{vehicle_id}", response_model=schemas.VehicleResponse, tags=["Vehicles"])
 def read_vehicle(vehicle_id: int, db: Session = Depends(get_db)):
     db_vehicle = db.query(models.Vehicle).get(vehicle_id)
     if not db_vehicle:
         raise HTTPException(status_code=404, detail=f"Vehicle with id {vehicle_id} could not be found")
     return db_vehicle
 
-@app.patch("/vehicles/{vehicle_id}/mileage", response_model=schemas.MileageResponse)
+@app.delete("/vehicles/{vehicle_id}", tags=["Vehicles"])
+def delete_vehicle(vehicle_id: int, db: Session = Depends(get_db)):
+    db_vehicle = db.query(models.Vehicle).get(vehicle_id)
+    if not db_vehicle:
+        raise HTTPException(status_code=404, detail=f"Vehicle with id {vehicle_id} could not be found")
+    db.delete(db_vehicle)
+    db.commit()
+    return {"messages": f"vehicle {db_vehicle.name} was succesfully deleted"}
+
+@app.patch("/vehicles/{vehicle_id}/mileage", response_model=schemas.UpdateResponse, tags=["Vehicle Detail"])
 def update_vehicle_mileage(vehicle_id: int, new_mileage: schemas.MileageUpdate, db: Session = Depends(get_db)):
     db_vehicle = db.query(models.Vehicle).get(vehicle_id)
     if not db_vehicle:
@@ -37,6 +46,26 @@ def update_vehicle_mileage(vehicle_id: int, new_mileage: schemas.MileageUpdate, 
     for field, value in update_dict.items():
         setattr(db_vehicle, field, value)
 
+    db.commit()
+    db.refresh(db_vehicle)
+    return db_vehicle
+
+@app.patch("/vehicles/{vehicle_id}/oil_update", response_model=schemas.UpdateResponse, tags=["Vehicle Detail"])
+def update_vehicle_oil(vehicle_id: int, db: Session = Depends(get_db)):
+    db_vehicle = db.query(models.Vehicle).get(vehicle_id)
+    if not db_vehicle:
+        raise HTTPException(status_code=404, detail=f"Vehicle with id {vehicle_id} could not be found")
+    db_vehicle.last_oil_change = db_vehicle.current_mileage
+    db.commit()
+    db.refresh(db_vehicle)
+    return db_vehicle
+
+@app.patch("/vehicles/{vehicle_id}/maintenance_update", response_model=schemas.UpdateResponse, tags=["Vehicle Detail"])
+def update_vehicle_maintenance(vehicle_id: int, db: Session = Depends(get_db)):
+    db_vehicle = db.query(models.Vehicle).get(vehicle_id)
+    if not db_vehicle:
+        raise HTTPException(status_code=404, detail=f"Vehicle with id {vehicle_id} could not be found")
+    db_vehicle.last_maintenance = db_vehicle.current_mileage
     db.commit()
     db.refresh(db_vehicle)
     return db_vehicle
--- schemas.py
+++ schemas.py
@@ -1,7 +1,6 @@
 from pydantic import BaseModel, Field
 from typing import Literal
 
-# --- SCHEMA 1: Buat input (Apa yang dikirim User) ---
 class VehicleBase(BaseModel):
     name: str = Field(min_length=3)
     cc: int = Field(ge=50)
@@ -10,34 +9,31 @@ class VehicleBase(BaseModel):
     transmission: Literal["Manual", "Semi Auto", "Automatic"]
 
 class VehicleCreate(VehicleBase):
-    # Data awal pas baru beli/daftar
     current_mileage: int = 0
     last_oil_change: int = 0
     last_maintenance: int = 0
 
-# --- SCHEMA 2: Buat output (Apa yang dikirim ke User) ---
 class VehicleResponse(VehicleBase):
     id: int
     current_mileage: int
     last_oil_change: int
     last_maintenance: int
     
-    # Ini manggil @property yang ada di models.py
     oil_change_interval: int
     maintenance_interval: int
     remaining_oil: int
     remaining_maint: int
 
     class Config:
-        # WAJIB! Biar Pydantic bisa narik data dari @property SQLAlchemy
         from_attributes = True
 
-# --- SCHEMA 3: Buat Update (Khusus buat nambah KM) ---
 class MileageUpdate(BaseModel):
     current_mileage: int = Field(ge=0, alias='new_mileage')
 
-class MileageResponse(VehicleBase):
+class UpdateResponse(VehicleBase):
     current_mileage: int
+    last_oil_change: int = 0
+    last_maintenance: int = 0
     remaining_oil: int
     remaining_maint: int
 
